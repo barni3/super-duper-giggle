6c06c26a4136207a13a230157f6b96e2761f93c0
barni3 <105896650+barni3@users.noreply.github.com>
2024-04-02 08:50:30 -0700

    Add files via upload

--- Game1.cs
+++ Game1.cs
@@ -12,24 +12,64 @@ namespace TheStrongest
         private SpriteBatch _spriteBatch;
         private Texture2D knightSprite;
         Texture2D backgroundSprite;
+        private Texture2D himsprite;
        Vector2  _position;
-       private Vector2 dir;
         Vector2 position;
-        int w;
-        int h;
+       private Vector2 dir;
+        private int spritewidth = 150;
+        private int spriteheight = 133;
+        private Vector2 velocity;
+        private Texture2D spriteimage;
+        private float spriteWidth;
+
+        private Enemy enemy;
+
         public float speed = 100;
+
+        private int screenWidth = 1920;
+
+        public int ScreenWidth
+        {
+            get { return screenWidth = 1920; }
+            set { screenWidth = value; }
+        }
+        private int screenHeight = 1080;
+
+        public int ScreenHeight
+        {
+            get { return screenHeight = 1080; }
+            set { screenHeight = value; }
+        }
+        public float spriteHeight
+        {
+            get
+            {
+                float scale = spriteWidth / spriteimage.Width;
+                return spriteimage.Height * scale;
+            }
+        }
+
+        public Rectangle PositionRectangle
+        {
+            get { return new Rectangle((int)position.X, (int)position.Y, (int)spriteWidth, (int)spriteHeight); }
+        }
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             _graphics.IsFullScreen = true;
-            w = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-            h = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-            _graphics.PreferredBackBufferWidth = w;
-            _graphics.PreferredBackBufferHeight = h;
+            _graphics.PreferredBackBufferWidth = screenWidth;
+            _graphics.PreferredBackBufferHeight = screenHeight;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            this.velocity = new Vector2(-1.0f, 5.0f);
+            this.position = position;
 
 
+
+
+            enemy = new Enemy(this, new Vector2(0, 0));
+            
             _graphics.ApplyChanges();
         }
 
@@ -47,6 +87,7 @@ namespace TheStrongest
             // TODO: use this.Content to load your game content here
             knightSprite = Content.Load<Texture2D>("1 IDLE_000");
             backgroundSprite = Content.Load<Texture2D>("pixel_art___dungeon_background__loopable__by_albertov_dbpx7j9");
+            himsprite = Content.Load<Texture2D>("1 IDLE_000");
         }
 
         protected override void Update(GameTime gameTime)
@@ -79,22 +120,32 @@ namespace TheStrongest
             var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             _position += dir * speed * deltaTime;
             // TODO: Add your update logic here
-            if (_position.X < -w)
+            if (_position.X < -1920)
             {
-                _position.X = 0;
+                _position.X = -1920;
             }
             if (_position.X > 0)
             {
-                _position.X = -w;
+                _position.X = 0;
             }
-            if (_position.Y < -h)
+            if (_position.Y < -1080)
             {
-                _position.Y = 0;
+                _position.Y =  - 1080;
             }
             if (_position.Y > 0)
             {
-                _position.Y = -h;
+                _position.Y = 0;
             }
+
+            //enemy movement
+            position += velocity;
+
+            if (position.Y < 0 || position.Y > (ScreenHeight - spriteHeight))
+            {
+                velocity.Y *= -1;
+            }
+
+
             base.Update(gameTime);
         }
 
@@ -105,9 +156,10 @@ namespace TheStrongest
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(backgroundSprite, _position, null, Color.White, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
-            _spriteBatch.Draw(knightSprite, new Vector2(w/2 - 150, h/2 - 133), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f); 
+            _spriteBatch.Draw(knightSprite, new Vector2(960 - spritewidth, 540 - spriteheight), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            _spriteBatch.Draw(himsprite, new Vector2(enemy.position.X, enemy.position.Y), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
             _spriteBatch.End();
-
+           
             base.Draw(gameTime);    
         }
     }
